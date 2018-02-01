86341ac40c719ab1d65493b5fcf04253da53e46b
AmitVermaGenp <amit.verma2e84c5@genpact.com>
2018-02-01 13:02:23 +0000

    Update helloWorld.java

--- helloWorld.java
+++ helloWorld.java
@@ -8,6 +8,9 @@ for(int i=0;i<10;i++)
  
  System.out.println("Hello World...."+i);
  System.out.println("*************.....Testing Deployment....**********");
+ System.out.println("*************.....Hi DevOps....**********");
+ 
+ 
 }
 
 }
