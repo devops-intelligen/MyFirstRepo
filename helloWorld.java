f97254f8bebc8d79ee71f0bc218a3b487aed7135
AmitVermaGenp <amit.verma2e84c5@genpact.com>
2018-02-01 13:52:32 +0000

    JI-2 "My first checkOutTest"
    JI-2 "My first checkOutTest"

--- helloWorld.java
+++ helloWorld.java
@@ -9,6 +9,7 @@ for(int i=0;i<10;i++)
  System.out.println("Hello World...."+i);
  System.out.println("*************.....Testing Deployment....**********");
  System.out.println("*************.....Hi DevOps....**********");
+ System.out.println("*************.....Hi DevOps square....**********");
  
  
 }
